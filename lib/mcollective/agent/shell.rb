9dccbde359c3be389eecf047c97dbc1e99f1c185
Richard Clamp <richardc@unixbeard.net>
2014-05-19 16:41:51 +0100

    Call processes with Process.spawn

--- lib/mcollective/agent/shell.rb
+++ lib/mcollective/agent/shell.rb
@@ -2,8 +2,70 @@ module MCollective
   module Agent
     class Shell<RPC::Agent
       action 'run' do
-        reply[:stdout] = "foo\n"
-        reply[:exitcode] = 0
+        result = run_command(request.data)
+        reply[:exitcode] = result[:exitcode]
+        reply[:stdout] = result[:stdout]
+        reply[:stderr] = result[:stderr]
+      end
+
+      private
+
+      def run_command(request = {})
+        reply = {}
+        stdout_rd, stdout_wr = IO.pipe
+        stderr_rd, stderr_wr = IO.pipe
+        options = {
+          #:chdir => "/",
+          :out => stdout_wr,
+          :err => stderr_wr,
+        }
+
+        Log.warn("spawning '#{request[:command]} with #{options.inspect}")
+
+        pid = Process.spawn(request[:command], options)
+        stdout_wr.close
+        stderr_wr.close
+        Log.warn("spawned pid #{pid}")
+
+        stdout = ''
+        stderr = ''
+        rd_fds = [ stdout_rd, stderr_rd ]
+        while !rd_fds.empty?
+          rds, = IO.select(rd_fds)
+          rds.each do |readable|
+            Log.warn("readable #{readable.inspect}")
+            case readable
+            when stdout_rd
+              stdout += stdout_rd.read
+              if stdout_rd.eof?
+                rd_fds.delete(stdout_rd)
+              end
+            when stderr_rd
+              stderr += stderr_rd.read
+              if stderr_rd.eof?
+                rd_fds.delete(stderr_rd)
+              end
+            else
+              Log.error("Unexpected fd #{readable.inspect}")
+            end
+          end
+        end
+
+        Log.warn("drained output")
+
+        waited_pid = Process.waitpid(pid)
+        Log.warn("waitpid(#{pid}) -> #{waited_pid}")
+
+        reply[:stdout] = stdout
+        reply[:stderr] = stderr
+
+        # On win32 $? doesn't seem to get set
+        if Util.windows?
+          reply[:exitcode] = 0
+        else
+          reply[:exitcode] = $?.exitstatus
+        end
+        return reply
       end
     end
   end
--- spec/unit/mcollective/agent/shell_spec.rb
+++ spec/unit/mcollective/agent/shell_spec.rb
@@ -7,10 +7,28 @@ module MCollective
       let(:agent) { MCollective::Test::LocalAgentTest.new('shell', :agent_file => agent_file).plugin }
 
       describe '#run' do
-        it 'should run cleanly' do
+        it 'should delegate to #run_command' do
+          agent.expects(:run_command).with({:command => 'echo foo'}).returns({
+            :exitcode => 0,
+            :stdout => "foo\n",
+            :stderr => '',
+          })
           result = agent.call(:run, :command => 'echo foo')
           result.should be_successful
-          result.should have_data_items(:exitcode => 0, :stdout => "foo\n")
+        end
+      end
+
+      describe '#run_command' do
+        it 'should run cleanly' do
+          result = agent.send(:run_command, :command => 'echo foo')
+          result[:exitcode].should == 0
+          result[:stdout].should == "foo\n"
+        end
+
+        it 'should cope with large amounts of output' do
+          result = agent.send(:run_command, :command => %{ruby -e '8000.times { puts "flirble wirble" }'})
+          result[:exitcode].should == 0
+          result[:stdout].should == "flirble wirble\n" * 8000
         end
       end
     end
